b14ee68b53068cadd8e3f39e58494d0415664ad0
Shuang Feng <sfeng11@ucmerced.edu>
2024-08-31 20:13:52 -0700

    add example

--- pyomo_example.py
+++ pyomo_example.py
@@ -1,9 +1,21 @@
 import pyomo.environ as pyo
+A = ['hammer', 'wrench', 'screwdriver', 'towel']
+b = {'hammer':8, 'wrench':3, 'screwdriver':6, 'towel':11}
+w = {'hammer':5, 'wrench':7, 'screwdriver':4, 'towel':3}
+
+W_max = 14
 
 model = pyo.ConcreteModel()
+model.x = pyo.Var(A, within=pyo.Binary)
+model.value = pyo.Objective(
+    expr = sum( b[i] * model.x[i] for i in A ),
+    sense = pyo.maximize )
+    
+model.weight = pyo.Constraint(
+    expr = sum( w[i] * model.x[i] for i in A ) <= W_max)
+    
+opt = pyo.SolverFactory('glpk')
 
-model.x = pyo.Var([1,2], domain=pyo.NonNegativeReals)
+result_obj = opt.solve(model,tee =True)
 
-model.OBJ = pyo.Objective(expr = 2*model.x[1] + 3*model.x[2])
-
-model.Constraint1 = pyo.Constraint(expr = 3*model.x[1] + 4*model.x[2] >= 1)
+model.pprint()
